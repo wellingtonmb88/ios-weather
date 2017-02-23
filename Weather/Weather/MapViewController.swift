e279990c78701684a710cd77b198e345f7610a5f
wellingtonmb88 <Wellington@WELLINGTONs-MBP.home>
2017-02-23 00:00:46 -0300

    Add search controller for mapview

--- Weather/Weather/MapViewController.swift
+++ Weather/Weather/MapViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 import MapKit
 import CoreData
 
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+
 class MapViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
     var annotations:[MKAnnotation] = []
+    
+    var resultSearchController:UISearchController? = nil
+    
+    var selectedPin:MKPlacemark? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +33,24 @@ class MapViewController: UIViewController, UIGestureRecognizerDelegate {
         lpgr.delaysTouchesBegan = true
         lpgr.delegate = self
         self.mapView.addGestureRecognizer(lpgr)
+        
+        let locationSearchTable =
+            storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
+        
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
     }
     
     @IBAction func saveCity(_ sender: Any) {
@@ -96,6 +122,32 @@ class MapViewController: UIViewController, UIGestureRecognizerDelegate {
             })
             
         }
+    }
+}
+
+extension MapViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        
+        guard let city = placemark.addressDictionary!["City"] as? String,
+            let country = placemark.addressDictionary!["Country"] as? String else {
+                return
+        }
+        
+        annotation.title = city
+        annotation.subtitle = country
+        
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true) 
+        self.annotations.append(annotation)
+        self.saveButton.isEnabled = true
     }
 }
 
